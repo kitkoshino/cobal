05e4627f68cfb0b965cbcdf859e7624017006548
cristianekoshino <cristianekoshino@gmail.com>
2020-07-02 12:29:25 +0100

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,29 @@ import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import Footer from './Components/Footer';
 import Employees from './Pages/Employees';
-
+import Employee from './Pages/Employees/Employee';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <main>
-        {/* <Home /> */}
-        <Employees />
-      </main>
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/employees">
+              <Employees />
+            </Route>
+            <Route exact path="/employee">
+              <Employee />
+            </Route>
+          </Switch>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Pages/Employees/Employee/index.jsx
+++ src/Pages/Employees/Employee/index.jsx
@@ -4,9 +4,46 @@ import './style.scss';
 function Employee() {
   return (
     <section className="employee-container">
-      <h1>New Employee</h1>
+      <h2>New Employee</h2>
+      <form action="">
+        <div className="form-input">
+          <label htmlFor="id">Employee ID:</label>
+          <input type="text" name="id" />
+        </div>
+        <div className="form-input">
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" />
+        </div>
+        <div className="form-input">
+          <label htmlFor="birthdate">Birthdate:</label>
+          <input type="text" name="birthdate" />
+        </div>
+        <div className="form-input">
+          <label htmlFor="address">Address:</label>
+          <input type="text" name="address" />
+        </div>
+        <div className="form-input">
+          <label htmlFor="status">Status:</label>
+          <input type="text" name="status" />
+        </div>
+        <div className="form-input">
+          <label htmlFor="position">Position:</label>
+          <input type="text" name="position" />
+        </div>
+        <div className="form-dates">
+          <div className="form-input">
+            <label htmlFor="created">Created:</label>
+            <input type="text" name="created" />
+          </div>
+          <div className="form-input">
+            <label htmlFor="updated">Updated:</label>
+            <input type="text" name="updated" />
+          </div>
+        </div>
+        <button>Submit</button>
+      </form>
     </section>
-  )
+  );
 }
 
 export default Employee;
